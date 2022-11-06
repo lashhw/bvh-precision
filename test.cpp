d7385dac393821451391ee2a13453ae0b5023e0c
Jacky Huang <lashhw81@gmail.com>
2022-11-06 11:36:37 +0800

    update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,56 +1,40 @@
 #include <mpfr.h>
 #include <limits>
+#include <cassert>
 #include <iostream>
+
+void print_mpfr(const mpfr_t &num) {
+    std::cout << mpfr_get_exp(num) << std::endl;
+    mpfr_out_str(stdout, 2, 0, num, GMP_RNDN);
+    std::cout << std::endl;
+    mpfr_out_str(stdout, 10, 0, num, GMP_RNDN);
+    std::cout << std::endl << std::endl;
+}
 
 int main() {
     mpfr_t one;
     mpfr_init2(one, 24);
-    mpfr_set_str(one, "1", 2, GMP_RNDN);
-
-    std::cout << mpfr_get_exp(one) << std::endl;
-    mpfr_out_str(stdout, 2, 0, one, GMP_RNDN);
-    std::cout << std::endl;
-    mpfr_out_str(stdout, 10, 0, one, GMP_RNDN);
-    std::cout << std::endl << std::endl;
+    assert(mpfr_set_str(one, "1", 2, GMP_RNDN) == 0);
+    print_mpfr(one);
 
     mpfr_t next;
     mpfr_init2(next, 24);
-    mpfr_set_str(next, "1", 2, GMP_RNDN);
+    assert(mpfr_set(next, one, GMP_RNDN) == 0);
     mpfr_nextabove(next);
-
-    std::cout << mpfr_get_exp(next) << std::endl;
-    mpfr_out_str(stdout, 2, 0, next, GMP_RNDN);
-    std::cout << std::endl;
-    mpfr_out_str(stdout, 10, 0, next, GMP_RNDN);
-    std::cout << std::endl << std::endl;
+    print_mpfr(next);
 
     mpfr_t epsilon;
     mpfr_init2(epsilon, 24);
-    mpfr_sub(epsilon, next, one, GMP_RNDN);
-
-    std::cout << mpfr_get_exp(epsilon) << std::endl;
-    mpfr_out_str(stdout, 2, 0, epsilon, GMP_RNDN);
-    std::cout << std::endl;
-    mpfr_out_str(stdout, 10, 0, epsilon, GMP_RNDN);
-    std::cout << std::endl << std::endl;
+    assert(mpfr_sub(epsilon, next, one, GMP_RNDN) == 0);
+    print_mpfr(epsilon);
 
     mpfr_t minimum;
     mpfr_init2(minimum, 24);
-    mpfr_set_d(minimum, std::numeric_limits<float>::min(), GMP_RNDN);
-
-    std::cout << mpfr_get_exp(minimum) << std::endl;
-    mpfr_out_str(stdout, 2, 0, minimum, GMP_RNDN);
-    std::cout << std::endl;
-    mpfr_out_str(stdout, 10, 0, minimum, GMP_RNDN);
-    std::cout << std::endl << std::endl;
+    assert(mpfr_set_d(minimum, std::numeric_limits<float>::min(), GMP_RNDN) == 0);
+    print_mpfr(minimum);
 
     mpfr_t maximum;
     mpfr_init2(maximum, 24);
-    mpfr_set_d(maximum, std::numeric_limits<float>::max(), GMP_RNDN);
-
-    std::cout << mpfr_get_exp(maximum) << std::endl;
-    mpfr_out_str(stdout, 2, 0, maximum, GMP_RNDN);
-    std::cout << std::endl;
-    mpfr_out_str(stdout, 10, 0, maximum, GMP_RNDN);
-    std::cout << std::endl << std::endl;
+    assert(mpfr_set_d(maximum, std::numeric_limits<float>::max(), GMP_RNDN) == 0);
+    print_mpfr(maximum);
 }
